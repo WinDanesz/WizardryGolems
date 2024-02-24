665893b1e126738216eaa4cba5864192f11858ff
WinDanesz <31292708+windanesz@users.noreply.github.com>
2024-02-24 22:33:41 +0100

    feat: Mushroom Golems have 50% chance to spawn a ring of mushrooms around them on death (depends on Ancient Spellcraft)

--- src/main/java/com/windanesz/wizardrygolems/entity/living/EntityMushroomGolemMinion.java
+++ src/main/java/com/windanesz/wizardrygolems/entity/living/EntityMushroomGolemMinion.java
@@ -1,10 +1,15 @@
 package com.windanesz.wizardrygolems.entity.living;
 
 import com.golems.main.ExtraGolems;
+import com.windanesz.wizardrygolems.integration.ASIntegration;
+import electroblob.wizardry.spell.Spell;
+import electroblob.wizardry.util.SpellModifiers;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.MobEffects;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
@@ -28,5 +33,16 @@ public class EntityMushroomGolemMinion extends EntityEarthGolemMinion {
 	@Override
 	public void onSuccessfulAttack(EntityLivingBase target) {
 		target.addPotionEffect(new PotionEffect(MobEffects.POISON, 100, 0));
+	}
+
+	@Override
+	public void onDeath(DamageSource cause) {
+		if (rand.nextBoolean() && ASIntegration.isLoaded()) {
+			Spell spell = Spell.get("ancientspellcraft:fairy_ring");
+			if (spell != null) {
+				spell.cast(world, this, EnumHand.MAIN_HAND, 0, getCaster(), new SpellModifiers());
+			}
+		}
+		super.onDeath(cause);
 	}
 }
